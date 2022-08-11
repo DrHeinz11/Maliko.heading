6c20878bd836d774d9dde2b6b972378e434e42b1
DrHeinz11 <franco.martin.1998m@gmail.com>
2022-08-11 00:05:46 -0300

    V1.1.3(add statics components)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import { Heading, Image, Text } from "@chakra-ui/react";
-import { useState } from "react";
 import logo from "./assets/Vector 632.png";
 import logoText from "./assets/Text-logo.svg";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <Image src={logo} />
