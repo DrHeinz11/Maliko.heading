9b5b7a33d79c6e300cc4e06608187b194a217fb8
DrHeinz11 <franco.martin.1998m@gmail.com>
2022-08-10 23:36:27 -0300

    V1.1.3(add statics components)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import { Heading, Text } from "@chakra-ui/react";
+import { Heading, Image, Text } from "@chakra-ui/react";
 import { useState } from "react";
+import logo from "./assets/Vector 632.png";
+import logoText from "./assets/Text-logo.svg";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
+      <Image src={logo} />
+      <Image src={logoText} />
       <Heading>
         Edit <code>src/App.jsx</code> and save to test HMR
       </Heading>
